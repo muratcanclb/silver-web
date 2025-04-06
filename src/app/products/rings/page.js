d1fbadfc3b2b9950fda6d6859923514121e4ae96
muratcanclb <muratcanclbi@gmail.com>
2025-04-06 18:55:17 +0300

    products page added

---
+++ src/app/products/rings/page.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const rings = () => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Yüzük</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">925 Ayarında saf gümüş malzemeden üretilen kaliteli yüzük modellerimiz</p>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/yuzuk-img/tk-tasli-camgobek-1-1.jpg" />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Taşlı Cam Göbek</h1>
+                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/yuzuk-img/tk-sade-camgobek-1-1.jpg" />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Sade Cam Göbek</h1>
+                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/yuzuk-img/tk-sade-tektas-1-1.jpg" />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Sade Tektaş</h1>
+                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/yuzuk-img/tk-tasli-baget-1.jpg" />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Taşlı Baget</h1>
+                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/yuzuk-img/tk-sade-baget-1.jpg" />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Sade Baget</h1>
+                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img alt="gallery" className="absolute inset-0 w-full h-full object-cover" src="/yuzuk-img/ck-tasli-baget-1-1.jpg" />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-100 bg-white opacity-0 hover:opacity-50">
+                <h1 className="title-font text-lg font-medium text-gray-500 mb-10">Çift Kol Taşlı Baget</h1>
+                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default rings
---
+++ src/app/products/set/page.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const set = () => {
+    return (
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-col text-center w-full mb-20">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Set</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">925 Ayarında saf gümüş malzemeden üretilen kaliteli set modellerimiz</p>
+                </div>
+                <div className="flex flex-wrap -m-4">
+                    <div className="lg:w-1/3 sm:w-1/2 p-4">
+                        <div className="flex relative">
+                            <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/takim-img/tk-sade-baget-1-2.png" />
+                            <div className="px-10 py-12 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Sade Baget</h1>
+                                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/3 sm:w-1/2 p-4">
+                        <div className="flex relative">
+                            <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/takim-img/tk-sade-camgobek-1-2.png" />
+                            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Sade Cam Göbek</h1>
+                                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/3 sm:w-1/2 p-4">
+                        <div className="flex relative">
+                            <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/takim-img/tk-sade-tambaget-1.jpg" />
+                            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Sade Tam Baget</h1>
+                                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/3 sm:w-1/2 p-4">
+                        <div className="flex relative">
+                            <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/takim-img/tk-tasli-baget-1.jpg" />
+                            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Taşlı Baget</h1>
+                                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/3 sm:w-2/3 p-4">
+                        <div className="flex relative">
+                            <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/takim-img/tk-tasli-papatya-1.jpg" />
+                            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Taşlı Papatya</h1>
+                                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/3 sm:w-2/3 p-4">
+                        <div className="flex relative">
+                            <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/takim-img/tk-tasli-trapez-1.jpg" />
+                            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-50">
+                                <h1 className="title-font text-lg font-medium text-gray-900 mb-10">Tek Kol Taşlı Trapez</h1>
+                                <p className="leading-relaxed">Ürünlerimiz Tamamen 925 Ayar Gümüş ve Ayar Garantilidir</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default set
